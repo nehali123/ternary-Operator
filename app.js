bbf2cbe084ceb741bc1d8328304c76ef0024d365
(neha ali) <159723021+nehali123@users.noreply.github.com>
2024-03-16 22:13:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+//condition or conditions ? expression 1 and :expression 2
+//if else statement
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "neha ali";
+let pswd = 12345;
+/*if (userName === "neha ali") {
+    console.log("you are successfully logged in");
+
+} else {
+     console.log("invalid userName") }
+     */
+userName == "ali" && pswd == 12345 //(&& two sides equal, or|| me one side equal hogi to run krega)
+    //userName == "ali" || pswd == 12345 
+    ? console.log("you are logged in")
+    : console.log("invalid userName and pasword");
---
+++ app.ts
@@ -0,0 +1,20 @@
+//condition or conditions ? expression 1 and :expression 2
+//if else statement
+
+import { clearScreenDown } from "readline";
+
+let userName: string = "neha ali";
+let pswd = 12345;
+
+/*if (userName === "neha ali") {
+    console.log("you are successfully logged in");
+
+} else {
+     console.log("invalid userName") }
+     */
+ //userName == "ali" && pswd == 12345//(&& two sides equal, or|| me one side equal hogi to run krega)
+ userName == "ali" || pswd == 12345 
+
+ 
+ ? console.log("you are logged in")
+ : console.log("invalid userName and pasword")
